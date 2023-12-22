4a1d848b6d73f4d70372c0e342c1a1af45f57632
eyadashrafkh <eyad.ashraf941@gmail.com>
2023-12-22 20:37:46 +0200

    Final commit

--- 7392.cpp
+++ 7392.cpp
@@ -1,69 +1,144 @@
 /* a simple producer/consumer using semaphores and threads
-
-   usage on Solaris:
-     gcc thisfile.c -lpthread -lposix4
-     a.out numIters
-
+   usage on Linux:
+     g++ -o 7392 7392.cpp -lpthread
+     ./7392
 */
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <semaphore.h>
-#define SHARED 0
+#include <unistd.h>
+#include <time.h>
+#include <iostream>
+#include <queue>
 
-void *Producer(void *);  /* the two threads */
-void *Consumer(void *);
+#define BUFFER_SIZE 3
 
-sem_t empty, full;    /* the global semaphores */
-int data;             /* shared buffer         */
-int numIters;
+void *mCounter(void *);
+void *mMonitor(void *);
+void *mCollector(void *);
+
+std::queue<int> buffer;
+int counter = 0;
+
+sem_t counterMutex;
+sem_t bufferMutex;
+sem_t bufferFull;
+sem_t bufferEmpty;
 
 /* main() -- read command line and create threads, then
              print result when the threads have quit */
 
-int main(int argc, char *argv[]) {
-  /* thread ids and attributes */
-  pthread_t pid, cid;  
-  pthread_attr_t attr;
-  pthread_attr_init(&attr);
-  pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+int main() {
+  // Seed random number generator
+  srandom(time(NULL));
+  // Initialize semaphores
+  sem_init(&counterMutex, 0, 1);
+  sem_init(&bufferMutex, 0, 1);
+  sem_init(&bufferFull, 0, 0);
+  sem_init(&bufferEmpty, 0, BUFFER_SIZE);
 
-  numIters = atoi(argv[1]);
-  sem_init(&empty, SHARED, 1);  /* sem empty = 1 */
-  sem_init(&full, SHARED, 0);   /* sem full = 0  */
+  // Create mCounter threads
+  const int numCounters = 5;
+  pthread_t counterThreads[numCounters];
+  int counterThreadIds[numCounters];
+  for (int i = 0; i < numCounters; i++) {
+    counterThreadIds[i] = i;
+    pthread_create(&counterThreads[i], NULL, mCounter, &counterThreadIds[i]);
+  }
 
-  printf("main started\n");
-  pthread_create(&pid, &attr, Producer, NULL);
-  pthread_create(&cid, &attr, Consumer, NULL);
-  pthread_join(pid, NULL);
-  pthread_join(cid, NULL);
-  printf("main done\n");
+  // Create mMonitor thread
+  pthread_t monitorThread;
+  pthread_create(&monitorThread, NULL, mMonitor, NULL);
+
+  // Create mCollector thread
+  pthread_t collectorThread;
+  pthread_create(&collectorThread, NULL, mCollector, NULL);
+
+  // Wait for all threads to finish
+  for (int i = 0; i < numCounters; i++) {
+    pthread_join(counterThreads[i], NULL);
+  }
+  pthread_join(monitorThread, NULL);
+  pthread_join(collectorThread, NULL);
+
+  // Destroy semaphores
+  sem_destroy(&counterMutex);
+  sem_destroy(&bufferMutex);
+  sem_destroy(&bufferFull);
+  sem_destroy(&bufferEmpty);
+
+  return 0;
 }
 
-/* deposit 1, ..., numIters into the data buffer */
-void *Producer(void *arg) {
-  int produced;
-  int value;
-  printf("Producer created\n");
-  for (produced = 0; produced < numIters; produced++) {
-    sem_getvalue(&empty, &value);
-    printf("Value in empty semaphore: %d\n", value);
-    sem_wait(&empty);
-    data = produced;
-    printf("DATA = %d\n",data);
-    sem_post(&full);
+void *mCounter(void *arg) {
+  int sleepPeriod = random() % 1000000; // microseconds
+  int threadId = *static_cast<int *>(arg);
+  while (true) {
+    usleep(sleepPeriod);
+    printf("Counter thread %d: received a message\n", threadId);
+
+    printf("Counter thread %d: waiting to write\n", threadId);
+    sem_wait(&counterMutex);
+
+    counter++;
+    printf("Counter thread %d: now adding to counter, counter value=%d\n", threadId, counter);
+    sem_post(&counterMutex);
   }
+
+  return NULL;
 }
 
-/* fetch numIters items from the buffer and sum them */
-void *Consumer(void *arg) {
-  int total = 0, consumed;
-  printf("Consumer created\n");
-  for (consumed = 0; consumed < numIters; consumed++) {
-    sem_wait(&full);
-    printf("Total: %d \tDATA:%d\n",total,data);
-    total = total+data;
-    sem_post(&empty);
+void *mMonitor(void *arg) {
+  int t = random() % 1000000; // Time interval in microseconds
+  while (true) {
+    usleep(t);
+
+    printf("Monitor thread: waiting to read counter\n");
+    sem_wait(&counterMutex);
+    int counterValue = counter;
+    printf("Monitor thread: reading a count value of %d\n", counterValue);
+    counter = 0;
+    sem_post(&counterMutex);
+
+    sem_wait(&bufferEmpty);
+    sem_wait(&bufferMutex);
+    if (buffer.size() < BUFFER_SIZE) {
+      if (counterValue == 0) {
+        printf("Monitor thread: nothing to write\n");
+      }
+      else{
+        printf("Monitor thread: writing to buffer at position %d\n", buffer.size());
+        buffer.push(counterValue);
+      }
+    } else {
+      printf("Monitor thread: Buffer full!!\n");
+    }
+    sem_post(&bufferMutex);
+    sem_post(&bufferFull);
   }
-  printf("for %d iterations, the total is %d\n", numIters, total);
+
+  return NULL;
+}
+
+
+void *mCollector(void *arg) {
+  int t = random() % 100000; // Time interval in microseconds
+  while (true) {
+    usleep(t);
+
+    sem_wait(&bufferFull);
+    sem_wait(&bufferMutex);
+    if (!buffer.empty()) {
+      int value = buffer.front();
+      buffer.pop();
+      printf("Collector thread: reading from the buffer at position %d\n", buffer.size());
+    } else {
+      printf("Collector thread: nothing is in the buffer!\n");
+    }
+    sem_post(&bufferMutex);
+    sem_post(&bufferEmpty);
+  }
+
+  return NULL;
 }
--- makefile
+++ makefile
@@ -1,2 +1,7 @@
-target:
-    g++ 7392.cpp -o main -lpthread -lrt
+run: 7392.cpp
+	g++ -o main $< -lpthread > output.txt
+	./main >> output.txt
+
+
+clean:
+	rm -f main output.txt
